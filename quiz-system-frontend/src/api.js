de6b9744f9249905b1e40d63082a7344a7c617ea
siddhardha <sk2943@njit.edu>
2024-10-25 12:17:04 -0400

    preparing for frontend deployment

--- quiz-system-frontend/src/api.js
+++ quiz-system-frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+// const API_URL = 'http://localhost:8080/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_URL,
